59fad66e1cdf33aa4e91c7f786547f31a26427cb
1carog <icarob.dossantos@outlook.com>
2025-08-24 15:44:23 -0300

    alteracao 7 index

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//document.addEventListener("DOMContentLoaded", function() {
+   // document.getElementById("meuFormulario").addEventListener("submit", function(event) {
+      //  event.preventDefault();
+    //    document.getElementById("mensagem").textContent = "Bom descanso!";
+  //  });
+//});
+
+
+//document.getElementById("meuFormulario").addEventListener("submit", function(event) {
+    //event.preventDefault(); //Impede envio real do formulário
+    //alert("Bom descanso!");
+    //document.getElementById("mensagem").textContent = "Bom descanso!";
+//});
